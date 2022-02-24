aaedb2c18c9c983f64299dd5079a19208ed7591a
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2022-02-24 12:47:38 -0600

    test: getCount method

---
+++ test/Counter.test.ts
@@ -0,0 +1,22 @@
+import chai from "chai";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import { ethers } from "hardhat";
+import { solidity } from "ethereum-waffle";
+
+const { expect } = chai.use(solidity);
+
+async function deployCounter() {
+  const Counter = await ethers.getContractFactory("Counter");
+  const counter = await Counter.deploy();
+  await counter.deployed();
+  return counter;
+}
+
+describe("Counter contract", () => {
+  it("has a method called 'getCount' which should initially return 0", async () => {
+    const counter = await deployCounter();
+    const count = await counter.getCount();
+    expect(count).to.eql(0);
+  });
+});
