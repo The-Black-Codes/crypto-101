c62c25994729099ee2a64aa84a6c269a1b2cd450
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2022-02-24 13:26:55 -0600

    chore: view our work in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { Suspense } from "react";
+import { WithWeb3 } from "./web3/WithWeb3";
+
+function AccountViewer({ accounts }) {
+  return (
+    <>
+      <h2>{JSON.stringify(accounts)}</h2>
+    </>
+  );
+}
+
+function Counter({ initialCount }) {
+  return (
+    <header>
+      <h2>{initialCount}</h2>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>Hello Web3</h1>
+      <Suspense fallback={<>Loading...</>}>
+        <WithWeb3>
+          <AccountViewer />
+          <Counter />
+        </WithWeb3>
+      </Suspense>
     </div>
   );
 }
